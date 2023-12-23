379bcf918c1e9c53f54e5b770aad4a477e858af1
Gabriel Cruz <gabrielcruz.mail@gmail.com>
2023-12-23 09:40:33 -0300

    [TDL] - Refatorando dropzones

--- www/public/assets/js/Kanban.js
+++ www/public/assets/js/Kanban.js
@@ -11,29 +11,29 @@ let Kanban = (function () {
 
     let listAllDropzones = function () {
         if (dropzones.length === 0) {
-            dropzones = document.querySelectorAll('.dropzone');
+            dropzones = $('.dropzone');
         }
 
         return dropzones;
     };
 
     let dragstart = function (event) {
-        listAllDropzones().forEach( dropzone => dropzone.classList.add('highlight'))
-        listAllDropzones().forEach( dropzone => dropzone.classList.add('dropzone-dragging'))
+        $.each(listAllDropzones(),  (index, dropzone)  => $(dropzone).addClass('highlight'));
+        $.each(listAllDropzones(),  (index, dropzone)  => $(dropzone).addClass('dropzone-dragging'));
     }
 
     let drag = function (event) {
     }
 
     let dragend = function (event) {
-        listAllDropzones().forEach( dropzone => dropzone.classList.remove('highlight'))
-        listAllDropzones().forEach( dropzone => dropzone.classList.remove('dropzone-dragging'))
+        $.each(listAllDropzones(),  (index, dropzone)  => $(dropzone).removeClass('highlight'));
+        $.each(listAllDropzones(),  (index, dropzone)  => $(dropzone).removeClass('dropzone-dragging'));
     }
 
     let addEventsToCard = function (card) {
         $(card).on('dragstart', dragstart);
-         $(card).on('drag', drag);
-         $(card).on('dragend', dragend);
+        $(card).on('drag', drag);
+        $(card).on('dragend', dragend);
     };
 
     let initCards = function () {
@@ -79,12 +79,12 @@ let Kanban = (function () {
     };
 
     let initDropzones = function () {
-        listAllDropzones().forEach( dropzone => {
-            dropzone.addEventListener('dragenter', dragenter)
-            dropzone.addEventListener('dragover', dragover)
-            dropzone.addEventListener('dragleave', dragleave)
-            dropzone.addEventListener('drop', drop)
-        })
+        $.each(listAllDropzones(),  function (index, dropzone) {
+            $(dropzone).on('dragenter', dragenter)
+            $(dropzone).on('dragover', dragover)
+            $(dropzone).on('dragleave', dragleave)
+            $(dropzone).on('drop', drop)
+        });
     };
 
     return {
